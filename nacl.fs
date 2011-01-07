09923dff7722c4ae02004385aad6a47b1846a4b7
bernd.paysan@gmx.de <bernd.paysan@gmx.de>
2011-01-07 23:07:41 +0000

    NaCl interface seems to work Caveat: NaCl as distributed isn't relocatible, so you have to add -fPIC as option to okcompilers/c; and for amd64, in crypto_onetimeauth/poly1305/amd64/auth.s, you have to delete the global labels, add the code in constant.s, and remove the .data/.rodata section specifier so that this all becomes local text labels (which are relocatible).  Furthermore, you have to add randombytes.o and cpucycles.o to libnacl.a (ar r libnacl.a randombytes.o cpucycles.o).

--- nacl.fs
+++ nacl.fs
@@ -1,6 +1,7 @@
 \ Interface to the NaCL library
 
 c-library nacl
+    s" nacl" add-lib
     \c #include <amd64/crypto_box.h>
     c-function crypto_box_keypair crypto_box_keypair a a -- void ( pk sk -- )
     c-function crypto_box crypto_box a a n a a a -- void ( c m mlen n pk sk -- )
@@ -16,3 +17,12 @@ c-library nacl
     \c #include <amd64/randombytes.h>
     c-function randombytes randombytes a n -- void ( addr n -- )
 end-c-library
+
+32 Constant PUBLICKEYBYTES
+32 Constant SECRETKEYBYTES
+32 Constant KEYBYTES
+32 Constant BEFORENMBYTES
+24 Constant NONCEBYTES
+32 Constant ZEROBYTES
+16 Constant BOXZEROBYTES
+
