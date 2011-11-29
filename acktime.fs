208bb8831ac5a294631470dae11c58547175f828
bernd <bernd>
2011-11-29 23:17:20 +0000

    gliding client average

--- acktime.fs
+++ acktime.fs
@@ -3,8 +3,12 @@
 variable oldserv
 variable oldclient
 variable first  first on
+variable clientavg
 
 : acktime ( serv client -- )
     first @ 0=
-    IF  over oldserv @ - . dup oldclient @ - . 2dup swap - . cr  THEN
+    IF
+	over oldserv @ - .
+	dup oldclient @ - dup . clientavg @ 4 rshift dup . - clientavg +!
+	2dup swap - . cr  THEN
     oldclient ! oldserv !  first off ;
