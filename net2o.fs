3cc50ea5df2f7a736a4ca1e04ee872c1727e654f
bernd <bernd>
2012-01-03 21:10:14 +0000

    smaller is more reliable

--- net2o.fs
+++ net2o.fs
@@ -328,12 +328,12 @@ Variable mapping-addr
 
 \ create context
 
-16 Value b2b-chunk#
+4 Value b2b-chunk#
 b2b-chunk# 2* 2* 1- Value tick-init \ ticks without ack
 #1000000 Value bandwidth-init \ 1µs/byte
 -1 Constant never
 -1 1 rshift Constant max-int64
-8 Value flybursts#
+2 Value flybursts#
 
 : ticks ( -- u )  ntime drop ;
 
