6982ee54d87c4c281687aa22b956f08a0c354f3f
bernd <bernd>
2012-01-13 00:06:05 +0000

    Wurstkessel test with 16 bits internal state

--- wurstkessel.fs
+++ wurstkessel.fs
@@ -22,6 +22,7 @@ cell 8 = [IF]
 
 8 64s Constant state#
 2 2*  Constant state#32
+1     Constant state#16
 
 $10 here $F and - allot 
 here state# allot \ source
@@ -226,7 +227,7 @@ DOES> swap 7 and cells + @ ;
 
 : update-state32 ( -- )
     wurst-state wurst-source state#32 xors
-    nextstate wurst-state state#32 move ;
+    nextstate wurst-state state#32 +!s ;
 
 Create permut#32 1 , 0 , \ permut length 2
 DOES> swap 1 and cells + @ ;
@@ -239,6 +240,29 @@ DOES> swap 1 and cells + @ ;
 : +entropy32 ( message -- message' )
     dup wurst-source state#32 xors  wurst-source over state#32 move
     state#32 + ;
+
+\ wurstkessel16 primitives - really degenerated case
+
+: wurst16 ( u1 u2 -- u3 )  >r 2* dup 8 rshift 1 and or $FF and r> xor ;
+: rngs16 'rngs + c@ ;
+
+: mix2bytes16 ( index n k -- b index' n ) wurst-state +
+    >r over wurst-source + c@ r> c@ xor -rot dup >r + $0 and r> ;
+
+: bytes2sum16 ( ud b -- ud' ) rngs16 wurst16 ;
+
+: update-state16 ( -- )
+    wurst-state c@ wurst-source c@ xor wurst-source c!
+    nextstate c@ wurst-state c@ + wurst-state c! ;
+
+: round16 ( n -- ) dup 1- swap
+    wurst-state c@ -rot
+    0 mix2bytes16 2>r bytes2sum16 2r> rot nextstate c!
+    2drop update-state16 ;
+
+: +entropy16 ( message -- message' )
+    dup c@ wurst-source c@ xor wurst-source c!  wurst-source c@ over c!
+    state#16 + ;
 
 \ fast mixing
 
