721a68131eb4b547c3e610ea045fe8e861441144
bernd <bernd>
2012-09-23 21:37:50 +0000

    Factored out debugging code into separate file

---
+++ debugging.fs
@@ -0,0 +1,111 @@
+\ debugging aids
+
+: debug)  ]] THEN [[ ;
+
+true [IF]
+    : debug: ( -- ) Create immediate false ,  DOES>
+	state @ IF  ]] Literal @ IF [[ ['] debug) assert-canary
+	ELSE  @ IF ['] noop assert-canary ELSE postpone (  THEN
+	THEN  ;
+[ELSE]
+    : debug: ( -- )  Create immediate false , DOES>
+	@ IF  ['] noop assert-canary  ELSE  postpone (  THEN ;
+[THEN]
+
+: hex[ ]] base @ >r hex [[ ; immediate
+: ]hex ]] r> base ! [[ ; immediate
+: x~~ ]] hex[ ~~ ]hex [[ ; immediate
+
+\ debugging switches
+
+debug: timing(
+debug: rate(
+debug: ratex(
+debug: deltat(
+debug: slack(
+debug: slk(
+debug: bursts(
+debug: resend(
+debug: track(
+debug: data(
+debug: cmd(
+debug: send(
+debug: firstack(
+debug: msg(
+debug: profile(
+debug: stat(
+debug: timeout(
+debug: ack(
+
+: +db ( "word" -- ) ' >body on ;
+
+Variable debug-eval
+
+: +debug ( -- )
+    BEGIN  argc @ 1 > WHILE
+	    1 arg s" +" string-prefix?  WHILE
+		1 arg debug-eval $!
+		s" db " debug-eval 1 $ins  s" (" debug-eval $+!
+		debug-eval $@ evaluate
+		shift-args
+	REPEAT  THEN ;
+
+\ timing measurements
+
+Variable last-tick
+
+: ticks-u ( -- u )  ntime drop ;
+: !@ ( value addr -- old-value )   dup @ >r ! r> ;
+
+: delta-t ( -- n )
+    ticks-u dup last-tick !@ - ;
+
+: timing ;
+[IFDEF] timing
+    Variable calc-time
+    Variable calc1-time
+    Variable send-time
+    Variable rec-time
+    Variable enc-time
+    Variable wait-time
+    
+    : init-timer ( -- )
+	ticks-u last-tick ! calc-time off send-time off rec-time off wait-time off
+	calc1-time off enc-time off ;
+    
+    : +calc  profile( delta-t calc-time +! ) ;
+    : +calc1 profile( delta-t calc1-time +! ) ;
+    : +send  profile( delta-t send-time +! ) ;
+    : +enc   profile( delta-t enc-time +! ) ;
+    : +rec   profile( delta-t rec-time +! ) ;
+    : +wait  profile( delta-t wait-time +! ) ;
+    
+    : .times ( -- ) profile(
+	." wait: " wait-time @ s>f 1n f* f. cr
+	." send: " send-time @ s>f 1n f* f. cr
+	." rec : " rec-time  @ s>f 1n f* f. cr
+	." enc : " enc-time  @ s>f 1n f* f. cr
+	." calc: " calc-time @ s>f 1n f* f. cr
+	." calc1: " calc1-time @ s>f 1n f* f. cr ) ;
+[ELSE]
+    ' noop alias +calc immediate
+    ' noop alias +calc1 immediate
+    ' noop alias +send immediate
+    ' noop alias +rec immediate
+    ' noop alias +wait immediate
+    ' noop alias +enc immediate
+    ' noop alias init-timer
+    ' noop alias .times
+[THEN]
+
+0 [IF]
+Local Variables:
+forth-local-words:
+    (
+     (("debug:" "field:" "sffield:" "dffield:" "64field:") non-immediate (font-lock-type-face . 2)
+      "[ \t\n]" t name (font-lock-variable-name-face . 3))
+     ("[a-z]+(" immediate (font-lock-comment-face . 1)
+      ")" nil comment (font-lock-comment-face . 1))
+    )
+End:
+[THEN]
--- net2o.fs
+++ net2o.fs
@@ -22,6 +22,7 @@ require nacl.fs
 require wurstkessel.fs
 require wurstkessel-init.fs
 require hash-table.fs
+require debugging.fs
 
 \ helper words
 
@@ -41,7 +42,6 @@ require hash-table.fs
 : umin! ( n addr -- )   >r r@ @ umin r> ! ;
 : umax! ( n addr -- )   >r r@ @ umax r> ! ;
 
-: !@ ( value addr -- old-value )   dup @ >r ! r> ;
 : max!@ ( n addr -- )   >r r@ @ max r> !@ ;
 
 [IFDEF] 64bit
@@ -126,29 +126,9 @@ Create reverse-table $100 0 [DO] [I] bitreverse8 c, [LOOP]
 
 [IFDEF] 64bit
     : ticks ( -- u )  ntime drop ;
-    ' ticks Alias ticks-u
 [ELSE]
     ' ntime Alias ticks
-    : ticks-u ( -- u )  ntime drop ;
 [THEN]
-
-\ debugging aids
-
-: debug)  ]] THEN [[ ;
-
-true [IF]
-    : debug: ( -- ) Create immediate false ,  DOES>
-	state @ IF  ]] Literal @ IF [[ ['] debug) assert-canary
-	ELSE  @ IF ['] noop assert-canary ELSE postpone (  THEN
-	THEN  ;
-[ELSE]
-    : debug: ( -- )  Create immediate false , DOES>
-	@ IF  ['] noop assert-canary  ELSE  postpone (  THEN ;
-[THEN]
-
-: hex[ ]] base @ >r hex [[ ; immediate
-: ]hex ]] r> base ! [[ ; immediate
-: x~~ ]] hex[ ~~ ]hex [[ ; immediate
 
 \ defined exceptions
 
@@ -163,85 +143,6 @@ s" string does not fit"          exception Constant !!stringfit!!
 s" ivs must be 64 bytes"         exception Constant !!ivs!!
 s" key+pubkey must be 32 bytes"  exception Constant !!keysize!!
 s" connection attempt timed out" exception Constant !!contimeout!!
-
-\ this is already defined in assertions
-
-debug: timing(
-debug: rate(
-debug: ratex(
-debug: deltat(
-debug: slack(
-debug: slk(
-debug: bursts(
-debug: resend(
-debug: track(
-debug: data(
-debug: cmd(
-debug: send(
-debug: firstack(
-debug: msg(
-debug: profile(
-debug: stat(
-debug: timeout(
-debug: ack(
-
-: +db ( "word" -- ) ' >body on ;
-
-Variable debug-eval
-
-: +debug ( -- )
-    BEGIN  argc @ 1 > WHILE
-	    1 arg s" +" string-prefix?  WHILE
-		1 arg debug-eval $!
-		s" db " debug-eval 1 $ins  s" (" debug-eval $+!
-		debug-eval $@ evaluate
-		shift-args
-	REPEAT  THEN ;
-
-\ timing measurements
-
-Variable last-tick
-
-: delta-t ( -- n )
-    ticks-u dup last-tick !@ - ;
-
-: timing ;
-[IFDEF] timing
-    Variable calc-time
-    Variable calc1-time
-    Variable send-time
-    Variable rec-time
-    Variable enc-time
-    Variable wait-time
-    
-    : init-timer ( -- )
-	ticks-u last-tick ! calc-time off send-time off rec-time off wait-time off
-	calc1-time off enc-time off ;
-    
-    : +calc  profile( delta-t calc-time +! ) ;
-    : +calc1 profile( delta-t calc1-time +! ) ;
-    : +send  profile( delta-t send-time +! ) ;
-    : +enc   profile( delta-t enc-time +! ) ;
-    : +rec   profile( delta-t rec-time +! ) ;
-    : +wait  profile( delta-t wait-time +! ) ;
-    
-    : .times ( -- ) profile(
-	." wait: " wait-time @ s>f 1n f* f. cr
-	." send: " send-time @ s>f 1n f* f. cr
-	." rec : " rec-time  @ s>f 1n f* f. cr
-	." enc : " enc-time  @ s>f 1n f* f. cr
-	." calc: " calc-time @ s>f 1n f* f. cr
-	." calc1: " calc1-time @ s>f 1n f* f. cr ) ;
-[ELSE]
-    ' noop alias +calc immediate
-    ' noop alias +calc1 immediate
-    ' noop alias +send immediate
-    ' noop alias +rec immediate
-    ' noop alias +wait immediate
-    ' noop alias +enc immediate
-    ' noop alias init-timer
-    ' noop alias .times
-[THEN]
 
 \ Create udp socket
 
