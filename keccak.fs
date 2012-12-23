eaee1dfbf1b07dd03ff473c792aab627b7a6993f
bernd <bernd>
2012-12-23 20:00:54 +0000

    Added keccak translation from readable keccak

---
+++ keccak.fs
@@ -0,0 +1,68 @@
+\ keccak.fs
+\ Forth version derived from "readable keccak" by Bernd Paysan
+\ 19-Nov-11  Markku-Juhani O. Saarinen <mjos@iki.fi>
+\ A baseline Keccak (3rd round) implementation.
+
+Create keccakf_rndc
+    $0000000000000001 , $0000000000008082 , $800000000000808a ,
+    $8000000080008000 , $000000000000808b , $0000000080000001 ,
+    $8000000080008081 , $8000000000008009 , $000000000000008a ,
+    $0000000000000088 , $0000000080008009 , $000000008000000a ,
+    $000000008000808b , $800000000000008b , $8000000000008089 ,
+    $8000000000008003 , $8000000000008002 , $8000000000000080 , 
+    $000000000000800a , $800000008000000a , $8000000080008081 ,
+    $8000000000008080 , $0000000080000001 , $8000000080008008 ,
+
+Create keccakf_rotc
+    1 c,  3 c,  6 c,  10 c, 15 c, 21 c, 28 c, 36 c, 45 c, 55 c, 2 c,  14 c, 
+    27 c, 41 c, 56 c, 8 c,  25 c, 43 c, 62 c, 18 c, 39 c, 61 c, 20 c, 44 c,
+
+Create keccakf_piln
+    10 c, 7 c,  11 c, 17 c, 18 c, 3 c, 5 c,  16 c, 8 c,  21 c, 24 c, 4 c, 
+    15 c, 23 c, 19 c, 13 c, 12 c, 2 c, 20 c, 14 c, 22 c, 9 c,  6 c,  1 c,
+
+\ update the state with given number of rounds
+
+Create bc 5 cells allot
+Create st 25 cells allot
+
+: 64rot1 ( x1 -- x2 )  dup 2* swap 0< - ;
+: 64rot ( x1 n -- x2 )  2dup lshift >r 64 swap - rshift r> or ;
+
+: theta1 ( -- )
+    5 0 DO
+	0 st i cells + 25 cells bounds DO  I @ xor  [ 5 cells ]L +LOOP
+	bc i cells + !
+    LOOP ;
+
+: theta2 ( -- )
+    5 0 DO
+	bc I 4 + 5 mod cells + @
+	bc I 1 + 5 mod cells + @ 64rot1 xor
+	st i cells + 25 cells bounds DO  dup I @ xor I !  [ 5 cells ]L +LOOP
+	drop
+    LOOP ;
+
+: rhopi ( -- )
+    st cell+ @
+    24 0 DO
+	keccakf_piln I + c@
+	cells st + dup @
+	rot keccakf_rotc I + c@ 64rot
+	rot !
+    LOOP drop ;
+
+: chi ( -- )
+    st 25 cells bounds DO
+	I bc 5 cells move
+	5 0 DO
+	    bc I 1+ 5 mod cells + @ bc 2 + 5 mod cells + @ and
+	    J I cells + dup >r @ xor r> !
+	LOOP
+    [ 5 cells ]L +LOOP ;
+
+: iota ( round -- )
+    cells keccakf_rndc + @ st @ xor st ! ;
+
+: keccakf ( rounds -- )
+    0 ?DO  theta1  theta2  rhopi  chi  I iota  LOOP ;
