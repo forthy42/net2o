5de584efe244073bc89be29c16d6d54b51aedd8a
bernd <bernd>
2013-01-30 00:06:13 +0000

    Moved automake to the right place

--- keccak/autogen.sh
+++ keccak/autogen.sh
@@ -2,7 +2,7 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
-automake --add-missing
 aclocal --install -I m4
 autoreconf --force --install --verbose "$srcdir"
+automake --add-missing
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
