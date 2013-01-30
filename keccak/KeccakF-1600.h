d2fe0fa0c4df0263202dba43b2b2d7bc492eb370
bernd <bernd>
2013-01-30 00:05:23 +0000

    Added KeccakF.h

---
+++ keccak/KeccakF-1600.h
@@ -0,0 +1,20 @@
+/* KeccakF interface */
+
+#ifndef _KeccakF_1600_h_
+#define _KeccakF_1600_h_
+
+typedef unsigned char UINT8;
+typedef unsigned short UINT16;
+typedef unsigned int UINT32;
+typedef unsigned long long int UINT64;
+typedef unsigned long keccak_state[25*sizeof(UINT64)/sizeof(unsigned long)];
+
+void KeccakInitialize();
+void KeccakF(keccak_state state);
+void KeccakInitializeState(keccak_state state);
+void KeccakExtract(const unsigned char *state, unsigned char *data, unsigned int laneCount);
+void KeccakAbsorb(keccak_state state, UINT64 *data, unsigned int laneCount);
+void KeccakEncrypt(keccak_state state, UINT64 *data, unsigned int laneCount);
+void KeccakDecrypt(keccak_state state, UINT64 *data, unsigned int laneCount);
+
+#endif
