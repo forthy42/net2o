250842c3c78a87062d966c6e784641472a50c9dd
bernd <bernd>
2014-06-18 22:59:15 +0000

    Added errors

---
+++ net2o-err.fs
@@ -0,0 +1,53 @@
+\ Copyright (C) 2010-2014   Bernd Paysan
+
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU Affero General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU Affero General Public License for more details.
+
+\ You should have received a copy of the GNU Affero General Public License
+\ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\ defined exceptions
+
+: throwcode ( addr u -- )  exception Create ,
+    [: >body @ >r ]] IF [[ r> ]] literal throw THEN [[ ;] set-compiler
+  DOES> ( flag -- ) @ and throw ;
+
+s" gap in file handles"          throwcode !!gap!!
+s" invalid file id"              throwcode !!fileid!!
+s" could not send"               throwcode !!send!!
+s" wrong packet size"            throwcode !!size!!
+s" no power of two"              throwcode !!pow2!!
+s" unimplemented net2o function" throwcode !!function!!
+s" too many commands"            throwcode !!commands!!
+s" string does not fit"          throwcode !!stringfit!!
+s" ivs must be 64 bytes"         throwcode !!ivs!!
+s" net2o timed out"              throwcode !!timeout!!
+s" maximum nesting reached"      throwcode !!maxnest!!
+s" nesting stack empty"          throwcode !!minnest!!
+s" invalid nest"                 throwcode !!nest!!
+s" invalid tmpnest"              throwcode !!tmpnest!!
+s" cookie recieved twice"        throwcode !!double-cookie!!
+s" code destination is 0"        throwcode !!no-dest!!
+s" no IP addr"                   throwcode !!no-addr!!
+s" absolute path not allowed!"   throwcode !!abs-path!!
+s" invalid packet destination"   throwcode !!inv-dest!!
+s" key+pubkey must be 32 bytes"  throwcode !!keysize!!
+s" unknown key"                  throwcode !!unknown-key!!
+s" wrong key"                    throwcode !!wrong-key!!
+s" no key file"                  throwcode !!nokey!!
+s" invalid Ed25519 key"          throwcode !!no-ed-key!!
+s" no temporary key"             throwcode !!no-tmpkey!!
+s" String stack full"            throwcode !!string-full!!
+s" String stack empty"           throwcode !!string-empty!!
+s" Unknown crypto function"      throwcode !!unknown-crypt!!
+s" Wrong revocation secret"      throwcode !!not-my-revsk!!
+s" krypto mem request too big"   throwcode !!kr-size!!
+s" secret storage size wrong"    throwcode !!sec-size!!
+
