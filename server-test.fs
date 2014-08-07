1be1c9c1003431f99da90d7707735b7d1543c56d
bernd <bernd>
2014-08-07 17:49:11 +0000

    Allow lax keys on server

--- server-test.fs
+++ server-test.fs
@@ -10,6 +10,7 @@ test-keys \ we want the test keys - never use this in production!
 ?nextarg [IF] s>number drop to net2o-port [THEN]
 
 i'm test
+strict-keys off \ server shouldn't have strict keys
 init-server
 server-loop
 
