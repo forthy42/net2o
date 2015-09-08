f99c536f9163b5d34acac425821e3b57c8e4e0fa
bernd <bernd>
2015-09-08 00:42:29 +0000

    sksig now different

--- tests/ed25519.fs
+++ tests/ed25519.fs
@@ -27,14 +27,15 @@ x" E09657D8C066FBAAD009A1189B3A7E418CE2002E73E6C799DA7A6F5D86CA5B76" skc swap mo
 x" C8B0514857E50524DEC94FB1157EF0BB0B89FFADA3A281FF2AE06F4BBD7EE671" stskc swap move
 skc pkc sk>pk pkc $20 x" 148777AA913CA970AD23E1C71B6B5C650B0448BA6DACEA5587ADFE13BA9262BB" str= 0= [IF] ." incorrect pubkey " pkc $20 xtype cr [THEN]
 stskc stpkc sk>pk stpkc $20 x" 301C3345E9756348DD442B03AAE186A73272ECF145D63C3A01DD7BBF7A3F24D7" str= 0= [IF] ." incorrect pubkey " stpkc $20 xtype cr [THEN]
+>sksig
 
 100 0 [do] skc stpkc pad ed-dh 2drop [loop] \ warmup for the CPU
 
 ." Test keypair "
 skc pkc 2dup sk>pk !time sk>pk .time cr
 ." Test signing "
-keccak0 "Test 123" >keccak keccak* sksig skc pkc ed-sign 2dup xtype
-x" 91749F6069108909E6663724379AF9ACF571C13EB273AD88BB071B503491041EAA7A87FE853B969CF87EC4C53851CD5DD0A3A7008C7AB094255B56E97D697301" str= 0= [IF] ." in" [THEN] ." correct sig "
+keccak0 "Test 123" >keccak keccak* sksig skc pkc ed-sign
+x" 422D393D79E24CFC1CBE42D8043F97057630D1E56DD7E8B57CE5FB8D483AE2A1D86EE12500F5856B559BFD781FE9D442CD502618FA94A69C9A41109AEB3E4B0C" str= 0= [IF] ." in" [THEN] ." correct sig "
 keccak0 "Test 123" >keccak keccak*
 sksig skc pkc !time ed-sign drop .time cr
 keccak0 "Test 123" >keccak keccak* dup pkc ed-verify drop
