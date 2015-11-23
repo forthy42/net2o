c5f2881ae3c9daa2cbd87fe40017520a6ea7be9a
bernd <bernd>
2015-11-23 01:08:07 +0000

    Fast wrapper for Android

---
+++ keccakfast.fs
@@ -0,0 +1,24 @@
+\ keccak fast wrapper
+
+\ Copyright (C) 2012-2015   Bernd Paysan
+
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU Affero General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU Affero General Public License for more details.
+
+\ You should have received a copy of the GNU Affero General Public License
+\ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require rec-scope.fs
+s" libkeccakfast.so" c-lib:open-path-lib drop
+
+c-library keccakfast
+    s" keccakfast" add-lib
+    include keccaklib.fs
+end-c-library
---
+++ threefishfast.fs
@@ -0,0 +1,24 @@
+\ threefish fast wrapper
+
+\ Copyright (C) 2012-2015   Bernd Paysan
+
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU Affero General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU Affero General Public License for more details.
+
+\ You should have received a copy of the GNU Affero General Public License
+\ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require rec-scope.fs
+s" libthreefishfast.so" c-lib:open-path-lib drop
+
+c-library threefishfast
+    s" threefishfast" add-lib
+    include threefishlib.fs
+end-c-library
