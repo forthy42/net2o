b222f50e12b8f43e988dbe8dc3b3f9dea9d5f876
bernd <bernd>
2015-12-21 01:07:12 +0000

    Do a renat on Android when the network changes

--- net2o-connect.fs
+++ net2o-connect.fs
@@ -202,11 +202,17 @@ Defer insert-addr ( o -- )
       cell+ $@ bounds ?DO
 	  I @ >o ret-beacon ping-addrs
 	  [: net2o-base:nop ret+beacon ;] punch-done-xt !
-	  ret-addr $10 erase  0 punch-addrs $[] @ insert-addr
+	  \ !!FIXME!! should maybe do a re-loopup?
+	  ret-addr $10 erase
+	  0 punch-addrs $[] @ insert-addr
 	  o to connection
 	  net2o-code gen-punchload gen-punch end-code o>
       cell +LOOP
     ;] #map ;
+
+[IFDEF] android-network
+    :noname  defers android-network renat ; is android-network
+[THEN]
 
 0 [IF]
 Local Variables:
