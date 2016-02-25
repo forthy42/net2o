427f82374d02c23673e871b1de9e31225a95d561
bernd <bernd>
2016-02-25 02:57:36 +0000

    Add a network specific wait for avalanches

--- net2o-helper.fs
+++ net2o-helper.fs
@@ -92,7 +92,10 @@ Defer dht-beacon
     also android also jni
     :noname  defers android-network
 	beacons @ IF
-	    network-info ?dup-IF  ]xref renat-all  THEN
+	    network-info ?dup-IF
+		dup .getType other-wait# mobil-wait# rot select
+		to avalanche-wait# ]xref
+		renat-all  THEN
 	THEN ;
     is android-network
     previous previous
