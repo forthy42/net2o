cdc3a1763ad06fe8958304a773b1fa1011cd98a1
bernd <bernd>
2016-04-18 20:21:59 +0000

    Added missing file

---
+++ net2o-dhtroot.fs
@@ -0,0 +1,20 @@
+\ default dhtroot data
+
+\ Copyright (C) 2016   Bernd Paysan
+
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU Affero General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU Affero General Public License for more details.
+
+\ You should have received a copy of the GNU Affero General Public License
+\ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+85" ~IIV0ZJeF4b8VIGKy;kXRvO*Z%#uVj>w`?m20n!c*_`(z=^#U_^cShx_>*%pu%=TW2JYz74d9LmgS%xC^mFi7GHGJ2fU|6V5=@_=|V?<pUYd)MQ}Gh(x%jN|0CsN|#@I{mF=Dxfzz_guXQYYMsol1ZRlwF^ol10Y)Pre*WH3YB_*P@2I1dG*gzgaEb2BFV*itIgSylm&Z7!5JeqpL350" dhtroot.n2o $!
+85" V=%}0%8((Zs63ZZca&!ZApl@5g8Xp_wSA~0ef#mK3LjR0p1000^_tO0j>c#ptnNW7mX@7@1(jN|0CsN|z}M)ff)MJt+=_IQO8&fY?yId>|&b+5iO_)yW>SUrVS|1E>XdEB`q+5O!@_+w)Pt@bWTT&Hlv;T&s;E5{bvZ3" dhtroot-addr$ $!
+
