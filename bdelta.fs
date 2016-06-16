0cde5c9aba3526cfa0fd150030bff7214508c1ec
bernd <bernd>
2016-06-16 14:12:05 +0000

    Small changes to bdelta.fs

--- bdelta.fs
+++ bdelta.fs
@@ -57,11 +57,12 @@ end-c-library
 Variable bfile1$
 Variable bfile2$
 
-: bslurp ( addr1 u1 addr2 u2 -- )
-    bfile2$ $slurp-file  bfile1$ $slurp-file ;
+: bslurp ( addr1 u1 addr2 u2 -- a b )
+    bfile2$ $slurp-file  bfile1$ $slurp-file
+    bfile1$ bfile2$ ;
 
-: bdelta-init ( addr1 u1 addr2 u2 -- o )
-    bfile1$ $@ 0  bfile2$ $@ 0  bdelta_init_alg ;
+: bdelta-init { a b -- o }
+    a $@ 0  b $@ 0  bdelta_init_alg ;
 
 : bd-pass { bs mins flags -- } ( o:b )
     o bs mins 0. flags bdelta_pass
@@ -86,20 +87,20 @@ Variable bfile2$
 : .ps ( x64 -- )
     p-tmp ps!+ p-tmp tuck - type ;
 
-: .diff ( o:b -- )
+: .diff ( b o:b -- ) { b }
     0 dup dup 64#0 64dup 64dup
     { p1' p2' fp 64^ p1 64^ p2 64^ numr }
     o bdelta_numMatches 0 ?DO
 	o i p1 p2 numr bdelta_getMatch
 	p2 64@ p2' n>64 64- 64dup .p 64>n >r
-	bfile2$ $@ fp /string r> umin dup >r type r> fp + to fp
+	b $@ fp /string r> umin dup >r type r> fp + to fp
 	p1 64@ p1' n>64 64- .ps
 	numr 64@ 64dup .p
 	64dup 64>n fp + to fp
 	64dup p1 64@ 64+ 64>n to p1'
 	p2 64@ 64+ 64>n to p2'
     LOOP
-    bfile2$ $@ fp /string dup IF
+    b $@ fp /string dup IF
 	dup n>64 .p type
     ELSE  2drop  THEN ;
 
@@ -108,21 +109,23 @@ Variable bdelta$
 : b$off ( -- )
     bfile1$ $off bfile2$ $off  bdelta$ $off ;
 
-: bdelta ( addr1 u1 addr2 u2 -- addr3 u3 ) bslurp
-    [: bdelta-init >o bd-passes .diff o bdelta_done_alg o>
-	 ;] bdelta$ $exec bdelta$ $@ ;
+: bdelta$2 ( a$ b$ -- )
+    tuck bdelta-init >o bd-passes .diff o bdelta_done_alg o> ;
 
-: .bpatch ( -- )
+: bdelta ( addr1 u1 addr2 u2 -- addr3 u3 ) bslurp
+    ['] bdelta$2 bdelta$ $exec bdelta$ $@ ;
+
+: bpatch$2 ( a$ diff$ -- )
     0 { fp }
-    bdelta$ $@ bounds U+DO
+    $@ bounds U+DO
 	I p@+ >r 64>n r> swap 2dup <info> type +
 	dup I' < IF
 	    ps@+ >r 64>n fp + to fp
-	    bfile1$ $@ fp safe/string
+	    dup $@ fp safe/string
 	    r> p@+ >r 64>n dup fp + to fp umin <warn> type r>
 	THEN
-    I - +LOOP <default> ;
+    I - +LOOP <default> drop ;
 
 : bpatch ( addr1 u1 addr2 u2 -- addr3 u3 )
-    bslurp 0 bfile2$ !@ bdelta$ ! ['] .bpatch bfile2$ $exec
-    bfile2$ $@ ;
+    bslurp ['] bpatch$2 bdelta$ $exec
+    bdelta$ $@ ;
