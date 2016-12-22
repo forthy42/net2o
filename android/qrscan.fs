e56a18cdd1348309caa53542dd846e3998ac7244
bernd <bernd>
2016-12-22 03:22:26 +0000

    Fix for ecc-ok? check

--- android/qrscan.fs
+++ android/qrscan.fs
@@ -188,7 +188,7 @@ $8 buffer: guessecc1
 	I     be-ul@ xor
 	I 4 + be-ul@ dup $AAAAAAAA and 1 rshift swap $55555555 and 2* or xor
     8 +LOOP ;
-: ecc-ok? ( addr u -- flag )  2dup 0 skip nip 0<> >r
+: ecc-ok? ( addr u -- flag )  2dup 0 skip nip 0<> >r  2dup
     >ecc-row  guessecc1     be-ul@ invert = >r
     >ecc-row2 guessecc1 4 + be-ul@        = r> and r> and ;
 
