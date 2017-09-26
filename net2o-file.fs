dabb603ac3bd1b564f9f36bf74c7dff40bef6dcb
Bernd Paysan <bernd.paysan@gmx.de>
2017-09-26 03:33:15 +0200

    Next try on fixing the buffer overrun problem

--- net2o-file.fs
+++ net2o-file.fs
@@ -349,7 +349,7 @@ scope{ mapc
     data-rmap with mapc fix-size raddr+ endwith residualwrite @ umin
     file( over data-rmap .mapc:dest-raddr - >r
     id id>addr? .fs-seek 64@ #10 64rshift 64>n >r )
-    id id>addr? .fs-write dup /back
+    id id>addr? .fs-write ( dup /back )
     file( dup IF ." file write: "
     id . r> hex. r> hex. dup hex. residualwrite @ hex. forth:cr
     ELSE  rdrop rdrop rdrop  THEN ) ;
@@ -362,13 +362,14 @@ scope{ mapc
     write-file# ? residualwrite @ hex. forth:cr ) back tail
     [: +calc fstates 0 { back tail states fails }
 	BEGIN  tail back u>  WHILE
-		back tail write-file# @ n2o:save-block  dup >blockalign +to back
+		back tail write-file# @ n2o:save-block  dup
+		>blockalign dup negate residualwrite +! +to back
 		IF 0 ELSE fails 1+ residualwrite off THEN to fails
 		residualwrite @ 0= IF
 		    write-file# file+ blocksize @ residualwrite !  THEN
 	    fails states u>= UNTIL
 	THEN
-	msg( ." Write end" cr ) +file
+	msg( ." Write end" cr ) +file  back data-rmap with mapc to dest-back endwith
 	fails states u>= IF  max/back  THEN \ if all files are done, align
     ;] file-sema c-section
     slurp( ."  left: " tail rdata-back@ drop data-rmap with mapc dest-raddr - endwith hex.
