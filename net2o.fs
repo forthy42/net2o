4f34ff86f9dfbe5fe725093c14ed8a1ea6a2961d
Bernd Paysan <bernd.paysan@gmx.de>
2018-03-16 15:28:16 +0100

    more work on curing resend problems

--- net2o.fs
+++ net2o.fs
@@ -1487,7 +1487,9 @@ forward n2o:see
     $error-id $off    \ no error id so far
     maxdata negate and >r inbuf packet-data r@ swap dup >r move
     r> r> swap
-    resend( ack@ .timeouts @ IF  <warn> dest-flags .dest-addr 2dup n2o:see <default>  THEN )
+    resend( o IF
+    ack@ .timeouts @ IF  ~~ <warn> 2dup n2o:see <default>  THEN
+    THEN )
     cmd-exec o IF  ( 0timeout ) o>  ELSE  rdrop  THEN
     remote? off ;
 ' handle-cmd rcode-class to handle
