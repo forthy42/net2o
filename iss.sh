01076d498927f7fadbba1944639e88eb71a41310
Bernd Paysan <bernd.paysan@gmx.de>
2018-08-11 21:09:59 +0200

    Unified 32/64 bit installer on Windows

--- iss.sh
+++ iss.sh
@@ -26,11 +26,19 @@ N2OVER=$(n2o version | cut -f1 -d' ' | cut -f2- -d-)
 machine=$(gforth --version 2>&1 | cut -f3 -d' ')
 SF=$(gforth -e 'cell 8 = [IF] ." 64" [THEN] bye')
 CYGWIN=cygwin$SF
+CYGWIN64=cygwin64
+CYGWIN32=cygwin
+X64=$(./gforth -e 'cell 8 = [IF] ." x64" [THEN] bye')
 
-for i in lib/gforth/$VERSION/$machine/libcc-named/*.la
+ln -fs /cygdrive/c/cygwin$(pwd)/lib/gforth/$VERSION/386 lib/gforth/$VERSION/
+
+for m in amd64 386
 do
-    sed "s/dependency_libs='.*'/dependency_libs=''/g" <$i >$i+
-    mv $i+ $i
+    for i in lib/gforth/$VERSION/$m/libcc-named/*.la
+    do
+	sed "s/dependency_libs='.*'/dependency_libs=''/g" <$i >$i+
+	mv $i+ $i
+    done
 done
 
 cat <<EOF
@@ -38,22 +46,23 @@ cat <<EOF
 ; Setup program is Inno Setup
 
 [Setup]
-AppName=net2o$SF
+AppName=net2o
 AppVersion=$N2OVER
-AppCopyright=Copyright © 2010-2016 Bernd Paysan
-DefaultDirName={pf}\net2o$SF
-DefaultGroupName=net2o$SF
+AppCopyright=Copyright © 2010-2018 Bernd Paysan
+DefaultDirName={pf}\net2o
+DefaultGroupName=net2o
 AllowNoIcons=1
 InfoBeforeFile=COPYING
 Compression=lzma
 DisableStartupPrompt=yes
 ChangesEnvironment=yes
-OutputBaseFilename=net2o$SF-$N2OVER
+OutputBaseFilename=net2o-$N2OVER
 AppPublisher=Bernd Paysan
 AppPublisherURL=http://fossil.net2o.de/net2o
 SignTool=signtool sign /sha1 F0DB1A7CC42249D69FCD403BDC6BA39C6BA4AC4B /fd SHA256 /tr http://timestamp.comodoca.com/authenticode /td SHA256 \$f
 SetupIconFile=net2o.ico
 UninstallDisplayIcon={app}\\net2o.ico
+ArchitecturesInstallIn64BitMode=$X64
 
 [Messages]
 WizardInfoBefore=License Agreement
@@ -72,7 +81,8 @@ $(make distfiles | tr ' ' '\n' | (while read i; do
 done) | sort -u | sed \
   -e 's:/:\\:g' \
   -e 's,^\(..*\)$,Name: "{app}\\\1",g')
-Name: "{app}\\..\\gforth$SF\\lib\\gforth\\$VERSION\\$machine\\libcc-named"
+ame: "{app}\\lib\\gforth\\$VERSION\\amd64\\libcc-named"; Check: Is64BitInstallMode
+Name: "{app}\\lib\\gforth\\$VERSION\\386\\libcc-named"; Check: not Is64BitInstallMode
 Name: "{app}\\..\\bin"
 Name: "{app}\\..\\tmp"; Permissions: users-modify
 
@@ -91,24 +101,20 @@ done) | sed \
   -e 's,^\([^\\]*\)$,Source: "\1"; DestDir: "{app}",g')
 Source: "net2o.ico"; DestDir: "{app}"
 $(for i in */.libs/*.dll; do
-echo "Source: \"$i\"; DestDir: \"{app}\\..\\gforth$SF\"" | tr '/' '\\'
+echo "Source: \"$i\"; DestDir: \"{app}\\..\\gforth\"" | tr '/' '\\'
 done)
-$(for i in lib/gforth/$VERSION/$machine/libcc-named/.libs/*.dll; do
-echo "Source: \"$i\"; DestDir: \"{app}\\..\\gforth$SF\\lib\\gforth\\$VERSION\\$machine\\libcc-named\\.libs\"" | tr '/' '\\'
-done)
-$(for i in lib/gforth/$VERSION/$machine/libcc-named/.libs/*; do
-echo "Source: \"$i\"; DestDir: \"{app}\\..\\gforth$SF\\lib\\gforth\\$VERSION\\$machine\\libcc-named\\.libs\"" | tr '/' '\\'
-done)
-$(for i in lib/gforth/$VERSION/$machine/libcc-named/*; do
-echo "Source: \"$i\"; DestDir: \"{app}\\..\\gforth$SF\\lib\\gforth\\$VERSION\\$machine\\libcc-named\"" | tr '/' '\\'
-done)
-Source: "c:\\$CYGWIN\\bin\\cygstdc++-6.dll"; DestDir: "{app}\\..\\gforth$SF"
+$(ls lib/gforth/$VERSION/amd64/libcc-named/*.la | sed -e 's:/:\\:g' -e 's,^\(..*\)$,Source: "\1"; DestDir: "{app}\\lib\\gforth\\'$VERSION'\\amd64\\libcc-named"; Check: Is64BitInstallMode,g')
+$(ls lib/gforth/$VERSION/amd64/libcc-named/.libs/*.dll | sed -e 's:/:\\:g' -e 's,^\(..*\)$,Source: "\1"; DestDir: "{app}\\lib\\gforth\\'$VERSION'\\amd64\\libcc-named\\.libs"; Check: Is64BitInstallMode,g')
+$(ls lib/gforth/$VERSION/386/libcc-named/*.la | sed -e 's,^\(..*\)$,Source: "C:\\cygwin'$(pwd)'\\\1"; DestDir: "{app}\\lib\\gforth\\'$VERSION'\\386\\libcc-named"; Check: not Is64BitInstallMode,g' | sed -e 's:/:\\:g')
+$(ls lib/gforth/$VERSION/386/libcc-named/.libs/*.dll | sed -e 's,^\(..*\)$,Source: "C:\\cygwin'$(pwd)'\\\1"; DestDir: "{app}\\lib\\gforth\\'$VERSION'\\386\\libcc-named\.libs"; Check: not Is64BitInstallMode,g' | sed -e 's:/:\\:g')
+Source: "c:\\$CYGWIN64\\bin\\cygstdc++-6.dll"; DestDir: "{app}\\..\\gforth"; Check: Is64BitInstallMode
+Source: "c:\\$CYGWIN32\\bin\\cygstdc++-6.dll"; DestDir: "{app}\\..\\gforth"; Check: not Is64BitInstallMode
 
 [Icons]
 ; Parameter quick reference:
 ;   "Icon title", "File name", "Parameters", "Working dir (can leave blank)",
 ;   "Custom icon filename (leave blank to use the default icon)", Icon index
-Name: "{group}\net2o"; Filename: "{app}\\..\\gforth$SF\\run.exe"; Parameters: "./env HOME='%HOMEDRIVE%%HOMEPATH%' ../gforth$SF/mintty ../gforth$SF/gforth-fast ./n2o cmd"; WorkingDir: "{app}"; IconFilename: "{app}\\net2o.ico"
+Name: "{group}\net2o"; Filename: "{app}\\..\\gforth\\run.exe"; Parameters: "./env HOME='%HOMEDRIVE%%HOMEPATH%' ../gforth/mintty ../gforth/gforth-fast ./n2o cmd"; WorkingDir: "{app}"; IconFilename: "{app}\\net2o.ico"
 
 [Run]
 
