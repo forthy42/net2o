54dc34f8b66df62e1bb3024edcb934e8ef70dc08
Bernd Paysan <bernd.paysan@gmx.de>
2019-11-21 18:54:15 +0100

    Try without auto-removal

--- dht.fs
+++ dht.fs
@@ -203,7 +203,7 @@ dht-class ' new static-a with-allocater constant dummy-dht
 
 : d#cleanup ( o:dht -- )
     k#size cell DO
-	dht-hash I + $@ bounds DO
+	dht-hash I + $@ bounds U+DO
 	    I @ IF  I $@ check-date IF  I $free  THEN  2drop  THEN
 	cell +LOOP  0 I del$cell
     cell +LOOP ;
