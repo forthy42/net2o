2f7d7ff365fc2d4c2738b43c0576e86cedfad981
Bernd Paysan <bernd.paysan@gmx.de>
2020-02-21 14:01:00 +0100

    Use .so on Linux instead of .la

--- net2o.fs
+++ net2o.fs
@@ -1,6 +1,6 @@
 \ net2o protocol stack
 
-\ Copyright © 2010-2015   Bernd Paysan
+\ Copyright © 2010-2020   Bernd Paysan
 
 \ This program is free software: you can redistribute it and/or modify
 \ it under the terms of the GNU Affero General Public License as published by
