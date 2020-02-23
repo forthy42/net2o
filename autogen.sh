4c9eea0587f2b448a77508687d831223beffa0a4
Bernd Paysan <bernd.paysan@gmx.de>
2020-02-23 23:27:43 +0100

    Add git-get ed25519-donna to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,21 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+function git-get {
+    echo "=== git clone $purl/$pname.git $* ==="
+    purl=$1
+    pname=$2
+    shift; shift
+    if [ -d $pname ]
+    then
+        (cd $pname; git pull -f)
+    else
+        git clone $purl/$pname.git $*
+    fi
+}
+
+git-get https://git.net2o.de/bernd ed25519-donna
+
 libtoolize --force --copy --install || glibtoolize --force --copy --install
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
--- wiki/commands.md
+++ wiki/commands.md
@@ -1,6 +1,6 @@
 # Commands #
 
-Version 0.9.7-20200213.
+Version 0.9.7-20200220.
 
 net2o separates data and commands.  Data is passed through to higher
 layers, commands are interpreted when they arrive.  For connection
