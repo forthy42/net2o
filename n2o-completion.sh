8d0e9fe0b92a07ccd60b1f40aa6cc8af8d0980de
Bernd Paysan <bernd.paysan@gmx.de>
2021-05-13 14:59:14 +0200

    keep net2o bindings in a net2o specific directory

--- n2o-completion.sh
+++ n2o-completion.sh
@@ -1,2 +1,2 @@
 # n2o bash and zsh completion
-complete -o default -W "$(n2o help 2>&1 | tail -n +2 | grep -v ^=== | cut -f1 -d' ')" n2o
+complete -o default -W "$(n2o help 2>&1 | tail -n +2 | grep -v ^=== | cut -f1 -d' ' | tr '|' ' ')" n2o
