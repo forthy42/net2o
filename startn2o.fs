ddc27f176a137ae0e4734776685fba459a5631b2
Bernd Paysan <bernd.paysan@gmx.de>
2021-08-28 16:04:34 +0200

    Start scripts for Android

--- startn2o.fs
+++ startn2o.fs
@@ -1,7 +1,7 @@
 \ Start net2o on android
 
 e? os-type s" linux-android" string-prefix? [IF] require starta.fs [THEN]
-page ." loading n2o..."
+page ." loading n2o..." key? drop
 warnings off \ no warnings please
 require n2o.fs
 :noname load-rc page get-me save-net2o-cmds set-net2o-cmds ; is bootmessage
--- startn2ogui.fs
+++ startn2ogui.fs
@@ -1,7 +1,10 @@
 \ Start net2o on android
 
 e? os-type s" linux-android" string-prefix? [IF] require starta.fs [THEN]
-page ." loading n2o..."
+page ." loading n2o..." key? drop
 warnings off \ no warnings please
 require n2o.fs
-:noname load-rc page save-net2o-cmds set-net2o-cmds n2o:gui bye ; is bootmessage
+:noname load-rc page save-net2o-cmds set-net2o-cmds
+    ." loading n2o:gui..." key? drop
+    n2o:gui ; is bootmessage
+' bye is 'quit
