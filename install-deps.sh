165fe74e5de006a9c67272ca83f8ea784eeba788
Bernd Paysan <bernd.paysan@gmx.de>
2025-03-09 15:38:10 +0100

    Add automatic packet generation

---
+++ install-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+which sudo || alias sudo=eval
+
+version=$1
+
+setup_debian() {
+wget -O - https://net2o.de/bernd@net2o.de-yubikey.pgp.asc | \
+    sudo gpg --dearmor -o /usr/share/keyrings/net2o-archive-keyring.gpg
+sudo sh -c "cat >/etc/apt/sources.list.d/net2o.list" <<EOF
+deb [signed-by=/usr/share/keyrings/net2o-archive-keyring.gpg] https://net2o.de/debian $version main
+EOF
+}
+
+install_debian() {
+    setup_debian()
+    sudo apt-get -y update
+    sudo apt-get -y install gforth gforth-minos2
