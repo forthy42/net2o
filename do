da1e124a4ede0d6360edeb4cc25ae6612af55c5e
Bernd Paysan <bernd.paysan@gmx.de>
2026-09-17 09:51:56 +0200

    Bump version number

--- do
+++ do
@@ -2,7 +2,7 @@
 
 echo "This script builds net2o from scratch"
 
-GFORTH=gforth-0.7.9_20260610
+GFORTH=gforth-0.7.9_20260916
 
 if [ "$(uname -o)" = "Cygwin" ]
 then
--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+PLATFORMS=${PLATFORMS:-linux/amd64,linux/arm64}
+
 eval $(n2o version | head -1 | while read NVERSION GFORTHVERSION; do GFORTHVERSION=${GFORTHVERSION%-*}; echo NVERSION=${NVERSION#*-} GFORTHVERSION=${GFORTHVERSION##*-}; done)
+
 sed -e "s/@VERSION@/$NVERSION/g" <Dockerfile.in >Dockerfile
 
-docker build --network host -t forthy42/net2o:latest .
-docker build --network host -f Dockerfile.gui -t forthy42/net2o-gui:latest .
-docker build --network host -f Dockerfile.gui+fonts -t forthy42/net2o-gui-fonts:latest .
+docker buildx build --platform $PLATFORMS --network host -t forthy42/net2o:latest . 2>net2o-build.log
+docker buildx build --platform $PLATFORMS --network host -f Dockerfile.gui -t forthy42/net2o-gui:latest . 2>net2o-gui-build.log
+docker buildx build --platform $PLATFORMS --network host -f Dockerfile.gui+fonts -t forthy42/net2o-gui-fonts:latest . 2>net2o-fonts-build.log
 if [ "$1" != "nopush" ]
 then
     docker push forthy42/net2o:latest
